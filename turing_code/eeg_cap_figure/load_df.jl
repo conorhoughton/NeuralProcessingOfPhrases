813918282ae9124ff8569a2b2e25e8392d8d6cf5
conor houghton <conor.houghton@bristol.ac.uk>
2021-10-09 14:45:38 +0100

    m

--- turing_code/eeg_cap_figure/load_df.jl
+++ turing_code/eeg_cap_figure/load_df.jl
@@ -44,9 +44,12 @@ else
     locationsDF=loadLocs()
     
     draw_size=20
+    lambda=1.055 #needs to be adjusted to get the circle circular
     dot_size=draw_size/3 #also divided by 10
     
-    thisPlot=plot(layer(electrodeValues,x=:x,y=:y,color=:means,size=[dot_size*mm]),layer(locationsDF,x=:x,y=:y,size=[1mm],color=[colorant"black"]),layer(x=[0.0],y=[0.0],size=[(draw_size*0.35)*cm]),layer(x=[0.0],y=[1.7],shape=[Shape.utriangle],size=[(draw_size*0.075)*cm]),Theme(background_color="white"))
+#    thisPlot=plot(layer(electrodeValues,x=:x,y=:y,color=:means,size=[dot_size*mm]),layer(locationsDF,x=:x,y=:y,size=[1mm],color=[colorant"black"]),layer(x=[0.0],y=[0.0],size=[(draw_size*0.36)*cm],color=[colorant"grey"]),layer(x=[0.0],y=[1.7],shape=[Shape.utriangle],size=[(draw_size*0.075)*cm],color=[colorant"grey"]),Theme(background_color="white"),Guide.xlabel(nothing),Guide.ylabel(nothing))
 
-    draw(PNG("test.png", draw_size*cm, draw_size*cm), thisPlot)
+    thisPlot=plot(layer(electrodeValues,x=:x,y=:y,color=:means,size=[dot_size*mm]),layer(x=[0.0],y=[0.0],size=[(draw_size*0.36)*cm],color=[colorant"grey"]),layer(x=[0.0],y=[1.7],shape=[Shape.utriangle],size=[(draw_size*0.075)*cm],color=[colorant"grey"]),Theme(background_color="white"),Guide.xlabel(nothing),Guide.ylabel(nothing))
+    
+    draw(PNG("test.png", 1.055*draw_size*cm, draw_size*cm), thisPlot)
 end
