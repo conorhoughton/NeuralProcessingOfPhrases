e5496776f44e79278bea9099c1bd9bbacf004491
conor houghton <conor.houghton@bristol.ac.uk>
2021-11-11 22:37:55 +0000

    m

---
+++ misc/itpc/plot_itpc.jl
@@ -0,0 +1,33 @@
+
+using Gadfly,Cairo,Fontconfig
+using CSV
+using DataFrames
+using Statistics
+
+condition = "anan"
+
+filename = "itpc_"*condition*"_all.txt"
+
+df = DataFrame(CSV.File(filename,delim=" ",header=false))
+
+df=df[:,Not(Between(:Column2,:Column5))]
+df=df[:,Not(:Column22)]
+
+oldNames=["Column"*string(i) for i in 6:21]
+participants=[string(i) for i in 1:16]
+
+rename!(df,[:Column1].=>[:frequency])
+rename!(df,oldNames.=>newNames)
+
+df=stack(df,2:17)
+
+layer1=layer(df,x=:frequency,y=:value,group=:variable,Geom.line)
+
+gdf = groupby(df, :frequency)
+gdf=combine(gdf, nrow, :value => mean => :mean)
+
+layer2=layer(gdf,x=:frequency,y=:mean,Geom.line,Theme(default_color="black",line_width=1mm))
+
+plt=plot(layer2,layer1,Theme(background_color="white"))
+
+draw(PNG("test.png",6cm,6cm),plt)
