500659af63579afb9da8b8f93d2df8140b6bb456
conor houghton <conor.houghton@bristol.ac.uk>
2021-11-14 11:14:59 +0000

    m

--- misc/plot_bundt.jl
+++ misc/plot_bundt.jl
@@ -10,17 +10,17 @@ plt1 = plot(z=(x,y) -> sqrt(x^2+y^2)*exp(-sqrt(x^2+y^2))/2π, xmin=[-3],
            style(major_label_font="CMU Serif",minor_label_font="CMU Serif", major_label_font_size=16pt,minor_label_font_size=14pt),
            Guide.colorkey(title="p(x,y)" ),Theme(background_color="white"))
 
-draw(SVG("bundt.svg",8cm,8cm),plt1)
+draw(PNG("bundt.png",8cm,8cm),plt1)
 
 set_default_plot_size(8cm, 4cm)
 
 plt2 = plot(x->x*exp(-x)/2π,0,3,style(major_label_font="CMU Serif",minor_label_font="CMU Serif",
                                       major_label_font_size=16pt,minor_label_font_size=14pt),
             Guide.yticks(ticks=[0.0,0.02,0.04,0.06]),
-            Guide.ylabel("p(r)"),Guide.xlabel("r",orientation=:horizontal),
+            Guide.ylabel("p(ρ)"),Guide.xlabel("ρ",orientation=:horizontal),
             Theme(background_color="white"))
 
-draw(SVG("bundt_profile.svg",8cm,4cm),plt2)
+draw(PNG("bundt_profile.png",8cm,4cm),plt2)
 
 #plts=vstack(plt2,plt1)
 
--- misc/plot_wrapped_cauchy.jl
+++ misc/plot_wrapped_cauchy.jl
@@ -1,3 +1,5 @@
+#there must be a better way to do this!
+
 using Gadfly
 using Cairo, Fontconfig
 using Compose
@@ -15,19 +17,23 @@ nPoints=200
 xticks = [0,49,99,149,199]
 labels = Dict(zip(xticks, ["-π","-π/2","0","π/2","π"]))
 
+pdf0=wrappedCauchy(0.5,0.0)
 pdf1=wrappedCauchy(1.0,0.0)
 pdf2=wrappedCauchy(2.0,0.0)
 pdf3=wrappedCauchy(5.0,0.0)
 
-pdfs=[[pdf1(θ) for θ in θs],[pdf2(θ) for θ in θs],[pdf3(θ) for θ in θs]]
+pdfs=[[pdf0(θ) for θ in θs],[pdf1(θ) for θ in θs],[pdf2(θ) for θ in θs],[pdf3(θ) for θ in θs]]
 
 plt=plot(pdfs, x=Row.index, y=Col.value, color=Col.index,
-         Scale.color_discrete_manual("red","purple","green"),
+         Scale.color_discrete_manual("blue","red","purple","green"),
          Stat.xticks(ticks=xticks),
          Scale.x_continuous(labels = x -> labels[x]),
-         Geom.line,Theme(background_color="white"),
-         Guide.colorkey(title="γ",labels=["1","2","5"]),
+         Geom.line,
+         style(major_label_font="CMU Serif",minor_label_font="CMU Serif"),
+         Theme(background_color="white"),
+         Guide.colorkey(title="γ",labels=["0.5","1.0","2.0","5.0"]),
          Guide.xlabel("θ"),Guide.ylabel("p(θ)")
          )
 
-draw(SVG("wrapped_cauchy.svg",8cm,12cm),plt)
+#draw(SVG("wrapped_cauchy.svg",8cm,12cm),plt)
+draw(PDF("wrapped_cauchy.pdf",8cm,8cm),plt)
